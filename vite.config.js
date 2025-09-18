b169df763fe89cb9f3aa895c47c3271534a7f8b2
debasmitpal12 <debasmitpal7@gmail.com>
2025-09-19 03:00:40 +0530

    Fix: add vercel.json for React Router support

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1600, // increase bundle size limit to avoid warnings
+    outDir: "dist",             // default build output directory
+    sourcemap: false            // you can set true if you want debugging
+  },
+  server: {
+    port: 5173,   // dev server port (optional, default 5173)
+    open: true,   // auto open browser when running `npm run dev`
+  },
 })
+
